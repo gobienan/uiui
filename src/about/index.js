470075e8e403279b29055dbd35af72c0d8d294a9
Gobie <gobie.nanthakumar@ada.com>
2021-11-07 13:19:50 +0100

    updated about page

--- src/about/index.js
+++ src/about/index.js
@@ -26,12 +26,12 @@ const render = () => {
   <div class="swiper-slide swiper-slide--last">
       <h1 class="uiui-headline">Hey, I’m Gobie 👋</h1>
       <br>
-      <p>I created this collection for others to play around! This collection will grow naturally over time. <br> If you have a cool example I am happy to add it as well :)</p>          
+      <p>I created this collection for others to play around with! This collection will grow naturally over time. <br> If you have a cool example I am happy to add it as well :)</p>          
       <br>
       <p>
       A little bit about me:
       <br>
-      I love to code and to build stuff with real world applications. Bootstrapping products is my way. <br>
+      I love to code and build stuff with real-world applications. Bootstrapping products is my way. <br>
       <a class="about-follow" href="https://twitter.com/gobienan" target="_blank" rel="noopener noreferrer">Follow</a> along if you would like 🥰<p/>
   </div>
 </div>
